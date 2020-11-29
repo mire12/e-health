dff27d0e03358ec7bdc5003b385b59945e0b0b4a
mire12 <miroo.koren@gmail.com>
2020-11-29 21:18:44 +0100

    added origin header

--- src/app/services/commmax-socket.service.ts
+++ src/app/services/commmax-socket.service.ts
@@ -17,22 +17,28 @@ export class CommmaxSocketService {
   });
   private corsHeaders: HttpHeaders;
 
+  private baseURL: string = 'https://www.itradix.online';
+
   constructor(private httpClient: HttpClient) {
     this.corsHeaders = new HttpHeaders({
       'Content-Type': 'text/plain; charset=utf-8',
       Accept: 'text/plain',
+      'Access-Control-Allow-Origin':'https://ehealth-ng-app.herokuapp.com',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, HEAD, OPTIONS',
       responseType: 'text',
     });
   }
 
   overVerziu(pID: String, evID: String, dID: String): Observable<any> {
+
+    const options = {
+      headers: this.corsHeaders
+    };
+
     return this.httpClient
       .get(
-        'https://www.itradix.online/ehealth/commmax/oververziu?pID=9c225a25baf2847828f299f46f22428571a4b305ad465e130ce58ac32e854fd0&dID=075441be7bc0cdbab6093bbaed5a25b2c06d33c6a2e74601cbea17d0885a75a5&evID=eVvyhladaj_zaznamy_preziadatela/db4d6732ef32dcd50cc2a7dbde3f89b4e67599017c0958c521c19c12d2b0cae920200717144413',
-        {
-          responseType: 'text',
-        }
+        `${this.baseURL}/ehealth/commmax/oververziu?pID=9c225a25baf2847828f299f46f22428571a4b305ad465e130ce58ac32e854fd0&dID=075441be7bc0cdbab6093bbaed5a25b2c06d33c6a2e74601cbea17d0885a75a5&evID=eVvyhladaj_zaznamy_preziadatela/db4d6732ef32dcd50cc2a7dbde3f89b4e67599017c0958c521c19c12d2b0cae920200717144413`,
+        options
       )
       .pipe(
         map((res) => {
