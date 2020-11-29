a05630d3a518fa734d27be26d87f5744e6d26d4c
mire12 <miroo.koren@gmail.com>
2020-11-30 00:20:26 +0100

    modified commax calling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+var cors = require('cors')
 const app = express();
-
+app.use(cors());
 app.use(express.static('./dist/e-health'));
 
 app.get('/*', function (req, res) {
