bd61ac3e0a88e6fa717e4c6d6a2495a359ddebc6
mire12 <miroo.koren@gmail.com>
2021-01-09 22:31:52 +0100

    added cors headers

--- src/app/services/commmax-socket.service.ts
+++ src/app/services/commmax-socket.service.ts
@@ -20,6 +20,9 @@ export class CommmaxSocketService {
 
   private HTTPOptions: Object = {
     headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, HEAD, OPTIONS',
+      'Access-Control-Allow-Headers': '*',
       'X-Skip-Interceptor': 'true',
     }),
     responseType: 'text',
